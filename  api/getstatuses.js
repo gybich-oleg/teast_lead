51c9179379f662e7a5f43eae7e24854fb0102883
gubich-oleg <gybich.oleg68@gmail.com>
2025-10-09 14:03:12 +0300

    add js

---  api/getstatuses.js
+++  api/getstatuses.js
@@ -5,8 +5,10 @@ export default async function handler(req, res) {
     }
 
     const CRM_URL = "https://crm.belmar.pro/api/v1/getstatuses";
-    const TOKEN = "ba67df6a-a17c-476f-8e95-bcdb75ed3958"; // заміни на актуальний!
+    const TOKEN = "ba67df6a-a17c-476f-8e95-bcdb75ed3958";
+
     try {
+        console.log("BODY:", req.body); // важливо!
         const crmRes = await fetch(CRM_URL, {
             method: "POST",
             headers: {
@@ -16,8 +18,10 @@ export default async function handler(req, res) {
             body: JSON.stringify(req.body),
         });
         const data = await crmRes.json();
+        console.log("CRM response:", data); // важливо!
         res.status(200).json(data);
     } catch (error) {
+        console.error(error);
         res.status(500).json({ error: "CRM API error" });
     }
 }
